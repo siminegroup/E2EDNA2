bba0e2cec5e636c9711494fbd94d3ecded39dfcc
ISDementyev <65086972+ISDementyev@users.noreply.github.com>
2021-08-19 16:44:00 -0400

    Added customAngles parameter, set to True

--- opendna.py
+++ opendna.py
@@ -352,7 +352,7 @@ class opendna():
         use LightDock to run docking and isolate good structures
         """
         printRecord('Docking')
-        buildPeptide(self.peptide)
+        buildPeptide(self.peptide, customAngles=True)
         ld = interfaces.ld(aptamer, peptide, self.params, self.i)
         ld.run()
         topScores = ld.topScores
