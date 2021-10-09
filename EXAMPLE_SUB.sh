7ea333b1563556f9dbb673e3e7a794d9bc95cfdd
Tao Liu <taoliu032@outlook.com>
2021-10-08 21:12:29 -0400

    implicit solvent - v2 - ambertools
    Make ambertools work compatibly with others, particularly numpy. Lots of try-and-check. The order of those commands should (probably matter).

--- EXAMPLE_SUB.sh
+++ EXAMPLE_SUB.sh
@@ -7,25 +7,43 @@
 #SBATCH --mail-user=YOUR EMAIL
 #SBATCH --mail-type=END
 
-export LD_LIBRARY_PATH= # put your MMB path here
+# put your MMB path here. Don't use ~
+export LD_LIBRARY_PATH="/home/taoliu/projects/def-simine/programs/MMB/Installer.2_14.Linux64"
 
 module load python/3.7
-module load StdEnv/2020
-module load gcc/9.3.0
-module load openmpi/4.0.3
-module load cuda/11.0
+#module load StdEnv/2020
+#module load gcc/9.3.0
+#module load openmpi/4.0.3
+#module load cuda/11.0
+
+# To load AmberTools21 (from https://docs.computecanada.ca/wiki/AMBER)
+# to use tleap to generate .prmtop file
+#module load StdEnv/2020 gcc/9.3.0 cuda/11.0 openmpi/4.0.3 ambertools/21
+#source $EBROOTAMBERTOOLS/amber.sh
+# Or load AmberTools20 (GPU version)
+module load StdEnv/2020  gcc/9.3.0  cuda/11.0  openmpi/4.0.3 amber/20.9-20.15
+
 module load python openmm
 
-virtualenv --no-download $SLURM_TMPDIR/env
+virtualenv --no-download $SLURM_TMPDIR/env  # need to load a Python module before running virtualenv
 source $SLURM_TMPDIR/env/bin/activate
 pip install --no-index --upgrade pip
 pip install --no-index MDAnalysis
 pip install --no-index lightdock
 pip install --no-index -r requirements.txt
-pip install numpy==1.20.2
+
+#pip install numpy==1.20.2
+#pip install --no-index numpy==1.20.2  # why didn't Michael use --no-index here?
+#pip install /cvmfs/soft.computecanada.ca/custom/python/wheelhouse/gentoo/generic/numpy-1.20.2+computecanada-cp37-cp37m-linux_x86_64.whl
+
+#ambertools21 and ambertools20 seems to contain numpy already: numpy-1.21.2
+pip install --upgrade numpy
+# Without this upgrade, numpy tends to give error for some packages such as mdtraj on beluga (2021-10-08)
+
 pip install ~/projects/def-simine/programs/opendnaWheels/seqfold-0.7.7-py3-none-any.whl
 pip install ~/projects/def-simine/programs/opendnaWheels/PeptideBuilder-1.1.0-py3-none-any.whl 
 pip install ~/projects/def-simine/programs/opendnaWheels/nupack-4.0.0.23-cp37-cp37m-linux_x86_64.whl
 python -c "import simtk.openmm"
 
-python ./main.py --run_num=0 --sequence='AACGCTTTTTGCGTT' --peptide='YQTQTNSPRRAR' --walltime=4
+python ./main.py --run_num=0 --sequence='CCCGGGCCCGGG' --peptide='YRRYRRYRRY' --walltime=4
+# python ./main.py --run_num=0 --sequence='AACGCTTTTTGCGTT' --peptide='YQTQTNSPRRAR' --walltime=4
