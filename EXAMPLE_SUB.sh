8fdcf879b7c0beeff4f4eeb6666a32a348c09c26
Tao Liu <taoliu032@outlook.com>
2022-01-10 16:44:05 -0500

    Update example_sub.sh

--- EXAMPLE_SUB.sh
+++ EXAMPLE_SUB.sh
@@ -37,13 +37,18 @@ pip install --no-index -r requirements.txt
 #pip install /cvmfs/soft.computecanada.ca/custom/python/wheelhouse/gentoo/generic/numpy-1.20.2+computecanada-cp37-cp37m-linux_x86_64.whl
 
 #ambertools21 and ambertools20 seems to contain numpy already: numpy-1.21.2
-pip install --upgrade numpy
+pip install --no-index --upgrade numpy
 # Without this upgrade, numpy tends to give error for some packages such as mdtraj on beluga (2021-10-08)
 
 pip install ~/projects/def-simine/programs/opendnaWheels/seqfold-0.7.7-py3-none-any.whl
 pip install ~/projects/def-simine/programs/opendnaWheels/PeptideBuilder-1.1.0-py3-none-any.whl 
+# https://pypi.org/project/PeptideBuilder/1.1.0/
+# https://www.wheelodex.org/projects/peptidebuilder/
 pip install ~/projects/def-simine/programs/opendnaWheels/nupack-4.0.0.23-cp37-cp37m-linux_x86_64.whl
 python -c "import simtk.openmm"
 
-python ./main.py --run_num=0 --sequence='CCCGGGCCCGGG' --peptide='YRRYRRYRRY' --walltime=4
-# python ./main.py --run_num=0 --sequence='AACGCTTTTTGCGTT' --peptide='YQTQTNSPRRAR' --walltime=4
+date -u
+# seq1:
+python ./main.py --run_num=1 --sequence='AACGCTTTTTGCGTA' --peptide='A' --walltime=10 --temperature=310 --pH=7.4 --ionicStrength=0.163 --Mg=0.05 --impSolv='HCT'
+
+date -u
