df41ec55cdc13d04f58879a15b123115cd915b1d
Tao Liu <taoliu032@outlook.com>
2022-01-12 18:43:16 -0500

    Update EXAMPLE_SUB.sh

--- EXAMPLE_SUB.sh
+++ EXAMPLE_SUB.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # For local run 
 
+# Create a conda environment ``myenv`` with python 3.7.11
+conda create -n myenv python=3.7.11
+# Activate the conda envs (using miniconda3 as an example)
+source activate ~/miniconda3/envs/myenv
+
 # Make sure to install all the required packages and their dependence
 pip install --upgrade pip
 pip install -r requirements.txt
@@ -8,13 +13,19 @@ pip install -r requirements.txt
 # MDAnalysis: https://www.mdanalysis.org/pages/installation_quick_start/
 conda config --add channels conda-forge
 conda install mdanalysis
+
+# OpenMM: http://docs.openmm.org/7.5.0/userguide/application.html#installing-openmm
+conda install -c conda-forge openmm
+
 # LightDock: # https://lightdock.org/
 pip install lightdock  
 
 # Seqfold: https://pypi.org/project/seqfold/
 pip install seqfold
+
 # NUPACK: download wheel from here: http://www.nupack.org/downloads
 pip install <downloaded wheel for your OS>
+
 # PeptideBuilder: # https://pypi.org/project/PeptideBuilder/1.1.0/ # https://www.wheelodex.org/projects/peptidebuilder/
 pip install PeptideBuilder
 
