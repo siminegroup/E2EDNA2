3adee9f21a4b403f3c5d3883ec15597905e1d25c
Tao Liu <taoliu032@outlook.com>
2022-01-21 09:58:06 -0500

    Update automate_tests.sh

--- automate_tests.sh
+++ automate_tests.sh
@@ -5,42 +5,42 @@
 printf "Starting the automated testing...\n"
 printf "===================================="
 
-printf "\nTesting mode #1: '2d structure'\n"
+printf "\nTESTING MODE #1: '2d structure'\n"
 python main.py --run_num=1 --mode='2d structure' --aptamerSeq='TAATGTTAATTG' --ligand='False' --ligandType='' --ligandSeq='' > run1.out
-printf "\nEnd of test #1, should see info of 2d structure in \"record.txt\" of working dir \"run1\"\n"
+printf "\nEND OF TEST #1: should see info of 2d structure in \"record.txt\" of working dir \"run1\"\n"
 printf "===================================="
 
-printf "\nTesting mode #2: '3d coarse'\n"
-python main.py --run_num=2 --mode='3d coarse' --aptamerSeq='TAATGTTAATTG' --ligand='False' --ligandType='' --ligandSeq='' > run2.out
-printf "\nEnd of test #2, should see info of 2d structure + MMB folded structure in \"run2\"\n"
-printf "===================================="
+# printf "\nTESTING MODE #2: '3d coarse'\n"
+# python main.py --run_num=2 --mode='3d coarse' --aptamerSeq='TAATGTTAATTG' --ligand='False' --ligandType='' --ligandSeq='' > run2.out
+# printf "\nEND OF TEST #2: should see info of 2d structure + MMB folded structure in \"run2\"\n"
+# printf "===================================="
 
-printf "\nTesting mode #3: '3d smooth'\n"
+printf "\nTESTING MODE #3: '3d smooth'\n"
 python main.py --run_num=3 --mode='3d smooth' --aptamerSeq='TAATGTTAATTG' --ligand='False' --ligandType='' --ligandSeq='' > run3.out
-printf "\nEnd of test #3, should see info of 2d structure + MMB folded structure + short MD relaxation in \"run3\"\n"
+printf "\nEND OF TEST #3: should see info of 2d structure + MMB folded structure + short MD relaxation in \"run3\"\n"
 printf "===================================="
 
-printf "\nTesting mode #4: 'coarse dock'\n"
+printf "\nTESTING MODE #4: 'coarse dock'\n"
 python main.py --run_num=4 --mode='coarse dock' --aptamerSeq='TAATGTTAATTG' --ligand='YQTQ.pdb' --ligandType='peptide' --ligandSeq='YQTQTNSPRRAR' > run4.out
-printf "\nEnd of test #4, should see info of 2d structure + MMB folded structure + docking by provided target ligand in \"run4\"\n"
+printf "\nEND OF TEST #4: should see info of 2d structure + MMB folded structure + docking by provided target ligand in \"run4\"\n"
 printf "===================================="
 
-printf "\nTesting mode #5: 'smooth dock'\n"
+printf "\nTESTING MODE #5: 'smooth dock'\n"
 python main.py --run_num=5 --mode='smooth dock' --aptamerSeq='TAATGTTAATTG' --ligand='YQTQ.pdb' --ligandType='peptide' --ligandSeq='YQTQTNSPRRAR' > run5.out
-printf "\nEnd of test #5, should see info of 2d structure + MMB folded structure + short MD relaxation + docked by provided target ligand in \"run5\"\n"
+printf "\nEND OF TEST #5: should see info of 2d structure + MMB folded structure + short MD relaxation + docked by provided target ligand in \"run5\"\n"
 printf "===================================="
 
-printf "\nTesting mode #6: 'free aptamer'\n"
-python main.py --run_num=6 --mode='free aptamer' --aptamerSeq='TAATGTTAATTG' --ligand='False' --ligandType='' --ligandSeq='' > run6.out
-printf "\nEnd of test #6, should see info of 2d structure + MMB folded structure + long MD sampling in \"run6\"\n"
-printf "===================================="
+# printf "\nTESTING MODE #6: 'free aptamer'\n"
+# python main.py --run_num=6 --mode='free aptamer' --aptamerSeq='TAATGTTAATTG' --ligand='False' --ligandType='' --ligandSeq='' > run6.out
+# printf "\nEND OF TEST #6: should see info of 2d structure + MMB folded structure + long MD sampling in \"run6\"\n"
+# printf "===================================="
 
-printf "\nTesting mode #7: 'full dock'\n"
-python main.py --run_num=7 --mode='full dock' --aptamerSeq='TAATGTTAATTG' --ligand='YQTQ.pdb' --ligandType='peptide' --ligandSeq='YQTQTNSPRRAR' > run7.out
-printf "\nEnd of test #7, should see info of 2d structure + MMB folded structure + long MD sampling + docked by provided target ligand in \"run7\"\n"
-printf "===================================="
+# printf "\nTESTING MODE #7: 'full dock'\n"
+# python main.py --run_num=7 --mode='full dock' --aptamerSeq='TAATGTTAATTG' --ligand='YQTQ.pdb' --ligandType='peptide' --ligandSeq='YQTQTNSPRRAR' > run7.out
+# printf "\nEND OF TEST #7: should see info of 2d structure + MMB folded structure + long MD sampling + docked by provided target ligand in \"run7\"\n"
+# printf "===================================="
 
-printf "\nTesting mode #8: 'full binding'\n"
-python main.py --run_num=8 --mode='full binding' --aptamerSeq='TAATGTTAATTG' --ligand='YQTQ.pdb' --ligandType='peptide' --ligandSeq='YQTQTNSPRRAR' > run8.out
-printf "\nEnd of test #8, should see info of 2d structure + MMB folded structure + long MD sampling + docked by provided target ligand + long MD sampling in \"run8\"\n"
-printf "====================================\n"
+# printf "\nTESTING MODE #8: 'full binding'\n"
+# python main.py --run_num=8 --mode='full binding' --aptamerSeq='TAATGTTAATTG' --ligand='YQTQ.pdb' --ligandType='peptide' --ligandSeq='YQTQTNSPRRAR' > run8.out
+# printf "\nEND OF TEST #8: should see info of 2d structure + MMB folded structure + long MD sampling + docked by provided target ligand + long MD sampling in \"run8\"\n"
+# printf "====================================\n"
